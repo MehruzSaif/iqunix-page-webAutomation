22959876053c6a08995bc405b4524bfb6ec93b13
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-02-27 05:37:10 +0600

    mamimized web

--- WebAutomation Practice/AutomationLoginPractice/src/test/java/com/bd/IqunixWebAutomation.java
+++ WebAutomation Practice/AutomationLoginPractice/src/test/java/com/bd/IqunixWebAutomation.java
@@ -1,10 +1,20 @@
 package com.bd;
 
-public class IqunixWebAutomation {
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class IqunixWebAutomation extends BaseDriver {
 
+	public String baseUrl = "https://iqunix.store/";
+
+	@Test
+	public void webTest() {
+		driver.get(baseUrl);
+		driver.manage().window().maximize();
+
+		if (driver.getTitle().contains("IQUNIX | High-performance and superb-design mechanical keyboards")) {
+			System.out.println(driver.getTitle());
+		} else {
+			System.out.println("Title didn't match");
+		}
 	}
-
 }
