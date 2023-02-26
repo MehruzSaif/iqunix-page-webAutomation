82b83d4e8f38d2534e61849476de0076822dc316
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-02-27 04:03:39 +0600

    added web driver manager

--- WebAutomation Practice/AutomationLoginPractice/src/test/java/com/bd/BaseDriver.java
+++ WebAutomation Practice/AutomationLoginPractice/src/test/java/com/bd/BaseDriver.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,6 +13,7 @@ public class BaseDriver {
 
 	public static WebDriver driver;
 
+	@BeforeSuite
 	public void start() {
 		String browser = System.getProperty("browser", "edge");
 
@@ -26,6 +29,7 @@ public class BaseDriver {
 		}
 	}
 
+	@AfterSuite
 	public void close() {
 		driver.close();
 	}
---
+++ WebAutomation Practice/AutomationLoginPractice/src/test/java/com/bd/IqunixWebAutomation.java
@@ -0,0 +1,10 @@
+package com.bd;
+
+public class IqunixWebAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
