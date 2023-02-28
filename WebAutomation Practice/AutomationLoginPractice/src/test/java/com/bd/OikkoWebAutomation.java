804f9d1677e9d9fe50669d9087d899d70f44023c
Mehruz Saif <mehruz.saif@g.bracu.ac.bd>
2023-03-01 03:44:57 +0600

    custom xpath

--- WebAutomation Practice/AutomationLoginPractice/src/test/java/com/bd/OikkoWebAutomation.java
+++ WebAutomation Practice/AutomationLoginPractice/src/test/java/com/bd/OikkoWebAutomation.java
@@ -1,19 +1,25 @@
 package com.bd;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class OikkoWebAutomation extends BaseDriver {
 
-	public String baseUrl = "https://www.oikko.com.bd/sign_in";
-	
+	public String baseUrl = "https://www.oikko.com.bd/";
+
 	@Test
 	public void webTest() throws InterruptedException {
 		driver.get(baseUrl);
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
-		
-		
-		
+
+		/*
+		 * XPath
+		 */
+		WebElement loginButton = driver.findElement(By.xpath("//a[@id='']"));
+		loginButton.click();
+
 		System.out.println(driver.getCurrentUrl());
 		Thread.sleep(5000);
 	}
